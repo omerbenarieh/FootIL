600bcb8e72446ac5e96492ab49134776e7831e1a
DorSonego <dorsonego123@gmail.com>
2023-08-18 18:53:14 +0300

    update

--- server.js
+++ server.js
@@ -28,16 +28,28 @@ mongoose
   .then(() => console.log('DB connection was successful! 😁'));
 
 // Some Tests for Socket comunicating with clients.
+// io.on('connection', socket => {
+//   console.log(`Client connected with id: ${socket.id}`);
+//   socket.on('send-msg', (message, room) => {
+//     if (room === '') {
+//       socket.broadcast.emit('receive-msg', message + ` ${socket.id}`);
+//     } else {
+//       socket.to(room).emit('receive-msg', message + ` from ${socket.id}`);
+//     }
+//   });
+// });
 io.on('connection', socket => {
   console.log(`Client connected with id: ${socket.id}`);
-  socket.on('send-msg', (message, room) => {
-    if (room === '') {
-      socket.broadcast.emit('receive-msg', message + ` ${socket.id}`);
-    } else {
-      socket.to(room).emit('receive-msg', message + ` from ${socket.id}`);
-    }
+
+  // Listen for messages from clients
+  socket.on('chat-message', message => {
+    // Process the message and generate a bot response
+    const botResponse = generateBotResponse(message);
+    // Emit the bot response to the client
+    socket.emit('chat-message', { text: botResponse, isBot: true });
   });
 });
+
 
 // Start Server
 const port = process.env.PORT;
