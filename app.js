af0a22b7d8ce6d7f8a4dcaaf00978875844f4cc9
DorSonego <dorsonego123@gmail.com>
2023-08-21 14:46:53 +0300

    commit

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set the ejs View Engine
-// app.set('view engine', 'ejs');
-// app.engine('ejs', require('ejs').__express);
-app.use(express.static(path.join(__dirname, 'front')));
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
+app.use(express.static(path.join(__dirname, 'front-new')));
 
 // Root
 app.get('/', (req, res) => {
