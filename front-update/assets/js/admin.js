60238dd4da9f4c800b7be2bd93aa3e6f92354190
omerbenarieh <omerrbenarieh@gmail.com>
2023-08-27 18:12:35 +0300

    admin page tables

--- front-update/assets/js/admin.js
+++ front-update/assets/js/admin.js
@@ -1,7 +1,11 @@
 import { getAllUsers } from './ajax/getAllUsers.js';
 import { renderUsers } from './handlers/renderUsers.js';
+import { getAllReservations } from './handlers/getAllReservations.js';
+import { renderAllReservations } from './handlers/renderAllReservations.js';
 
 $(document).ready(async function () {
   const users = await getAllUsers();
   renderUsers(users);
+  const reservations = await getAllReservations();
+  renderAllReservations(reservations);
 });
