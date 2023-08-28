1502647badc75dfa5bcd22a8223ec0a447410b59
ilaycohen2712 <ilaycohen1998@gmail.com>
2023-08-28 18:18:33 +0300

    group by

--- front-update/assets/js/admin.js
+++ front-update/assets/js/admin.js
@@ -8,4 +8,21 @@ $(document).ready(async function () {
   renderUsers(users);
   const reservations = await getAllReservations();
   renderAllReservations(reservations);
+  // reservations.forEach((reservation, index) => {
+  //   const date = reservation.dateOfReservation;
+  //   const totalprice = reservation.totalPrice;
+  //   if (!datelist.some(reservation => reservation.dateOfReservation == date)) {
+  //     datelist.push(date.split('T')[0]);
+  //   }
+  // });
+  const groupedByDate = reservations.reduce((result, reservation) => {
+    const { date, totalPrice } = reservation;
+    if (!result[date]) {
+      result[date] = { date, total: 0 };
+    }
+    result[date].total += totalPrice;
+    return result;
+  }, {});
+  const groupedData = Object.values(groupedByDate);
+  console.log(groupedData);
 });
